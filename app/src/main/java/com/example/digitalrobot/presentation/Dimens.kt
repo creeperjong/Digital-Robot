388e78b5b7284507e09614bcbf4a7c60257327a2
creeperjong <leogood50203@gmail.com>
2024-09-04 00:18:34 +0800

    add startup page

---
+++ app/src/main/java/com/example/digitalrobot/presentation/Dimens.kt
@@ -0,0 +1,11 @@
+package com.example.digitalrobot.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    val SmallPadding = 12.dp
+
+    val MediumPadding = 24.dp
+
+}
---
+++ app/src/main/java/com/example/digitalrobot/presentation/startup/StartUpScreen.kt
@@ -0,0 +1,77 @@
+package com.example.digitalrobot.presentation.startup
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.digitalrobot.presentation.Dimens.MediumPadding
+import com.example.digitalrobot.presentation.Dimens.SmallPadding
+import com.example.digitalrobot.ui.theme.DigitalRobotTheme
+
+@Composable
+fun StartUpScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(MediumPadding)
+            .statusBarsPadding()
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "Please enter MAC address or scan QR code ...")
+            },
+            modifier = Modifier.fillMaxWidth().padding(SmallPadding)
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ),
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(SmallPadding)
+        ) {
+            Text(text = "Scan QR Code")
+        }
+        Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ),
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(SmallPadding)
+        ) {
+            Text(text = "Done")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun StartUpScreenPreview() {
+    DigitalRobotTheme {
+        StartUpScreen()
+    }
+}
